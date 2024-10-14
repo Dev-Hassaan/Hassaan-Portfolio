241bc422b3b106a4d635c6bad26b1d4e8c3e3f72
Hassaan <syedhassaan720@gmail.com>
2024-10-15 00:00:38 +0500

    Head Component has been added

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,12 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import  Header  from "./components/header/header.jsx";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div className="flex flex-col justify-center items-center h-screen">
-        <h1 className='text-8xl'>🔥 Hello DaisyUI</h1>
-      </div>
+      <Header></Header>
     </>
   )
 }
